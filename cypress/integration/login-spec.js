f128a760d1f2e6dc54145c8a6c774a73803bb1df
sandking-number1 <Alfred.J9412@gmail.com>
2017-10-23 10:48:33 +0200

    .

--- cypress/integration/login-spec.js
+++ cypress/integration/login-spec.js
@@ -4,19 +4,19 @@ import { logout } from '../utils'
 describe('Login page', function(){
     const UI_BASE_PATH = Cypress.env("baseUrl");
 
-    it('Login with invalid user', function() {
-        cy.visit(UI_BASE_PATH);
-
-        cy.get('input[name=username]').type(invalidUser.username);
-
-        // {enter} causes the form to submit
-        cy.get('input[name=password]').type(`${invalidUser.password}{enter}`);
-
-        cy.url().should('eq', UI_BASE_PATH + '/login');
-
-        cy.get('#login-error-message').should('have.class', 'alert alert-danger');
-
-    });
+    // it('Login with invalid user', function() {
+    //     cy.visit(UI_BASE_PATH);
+    //
+    //     cy.get('input[name=username]').type(invalidUser.username);
+    //
+    //     // {enter} causes the form to submit
+    //     cy.get('input[name=password]').type(`${invalidUser.password}{enter}`);
+    //
+    //     cy.url().should('eq', UI_BASE_PATH + '/login');
+    //
+    //     cy.get('#login-error-message').should('have.class', 'alert alert-danger');
+    //
+    // });
 
     it('Login with valid user', function() {
         cy.visit(UI_BASE_PATH);
