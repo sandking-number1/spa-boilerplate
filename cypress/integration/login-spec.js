2b308a29a1669e9d66f7ce215d7b04e90205890a
sandking-number1 <Alfred.J9412@gmail.com>
2017-10-23 10:43:40 +0200

    a

--- cypress/integration/login-spec.js
+++ cypress/integration/login-spec.js
@@ -4,21 +4,6 @@ import { logout } from '../utils'
 describe('Login page', function(){
     const UI_BASE_PATH = Cypress.env("baseUrl");
 
-    // it('Login with valid user', function() {
-    //     cy.visit(UI_BASE_PATH);
-    //
-    //     // we should be redirected to /login
-    //     cy.url().should('eq', UI_BASE_PATH + '/login');
-    //
-    //     cy.get('input[name=username]').type(validUser.username);
-    //
-    //     // {enter} causes the form to submit
-    //     cy.get('input[name=password]').type(`${validUser.password}{enter}`);
-    //
-    //     // we should be redirected to /
-    //     cy.url().should('eq',  UI_BASE_PATH + '/');
-    // });
-
     it('Login with invalid user', function() {
         cy.visit(UI_BASE_PATH);
 
@@ -31,6 +16,21 @@ describe('Login page', function(){
 
         cy.get('#login-error-message').should('have.class', 'alert alert-danger');
 
+    });
+
+    it('Login with valid user', function() {
+        cy.visit(UI_BASE_PATH);
+
+        // we should be redirected to /login
+        cy.url().should('eq', UI_BASE_PATH + '/login');
+
+        cy.get('input[name=username]').type(validUser.username);
+
+        // {enter} causes the form to submit
+        cy.get('input[name=password]').type(`${validUser.password}{enter}`);
+
+        // we should be redirected to /
+        cy.url().should('eq',  UI_BASE_PATH + '/');
     });
 
 });
